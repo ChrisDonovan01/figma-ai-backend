a740de9e33e081a32a52be978cb4435f2d170192
Chris Donovan <donovan68@mac.mynetworksettings.com>
2025-07-14 13:25:31 -0400

    Replaced Node.js code with minimal implementation for debugging

--- api/index.js
+++ api/index.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-
-// Root Endpoint
-app.get('/', (req, res) => {
-  console.log('Root endpoint accessed.');
-  res.status(200).send('Root endpoint is working!');
-});
-
-module.exports = serverless(app);
+module.exports = (req, res) => {
+  console.log("Root endpoint accessed.");
+  res.end("Root endpoint is working!");
+};
